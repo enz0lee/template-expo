0046516b7c86ee3f9e6d009d4192ac3a2c398286
Enzo Lee <tuantai0625@gmail.com>
2026-03-07 20:13:24 +0700

    chore: integrate @tanstack/eslint-plugin-query into ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import pluginQuery from '@tanstack/eslint-plugin-query'
 import expoConfig from 'eslint-config-expo/flat.js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import reactCompiler from 'eslint-plugin-react-compiler'
@@ -5,6 +6,7 @@ import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   expoConfig,
+  ...pluginQuery.configs['flat/recommended'],
   reactCompiler.configs.recommended,
   eslintPluginPrettierRecommended,
   {
